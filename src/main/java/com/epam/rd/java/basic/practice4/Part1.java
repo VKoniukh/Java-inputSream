7384b62963af2849945a3fe473aa9f0d601739b6
Volodymyr <konuhvladimir1@gmail.com>
2021-07-19 16:27:30 +0300

    2

--- src/main/java/com/epam/rd/java/basic/practice4/Part1.java
+++ src/main/java/com/epam/rd/java/basic/practice4/Part1.java
@@ -2,7 +2,6 @@ package com.epam.rd.java.basic.practice4;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -10,15 +9,15 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Part1 {
-    public static final String ONE_WORD = "[a-zA-Z0-9][\\wа-яёА-ЯЁ]+/ig{4,}";
+    public static final String ONE_WORD = "[\\wа-яА-Я]{4,}";
+
+    static Logger logger;
 
     public static void main(String[] args) {
         String outputText = getInput("part1.txt");
-        outputText = sb1(outputText);
+        outputText = ChangeInput(outputText);
         System.out.println(outputText);
     }
-
-    static Logger logger;
 
     public static String getInput(String filePath) {
         StringBuilder sb = new StringBuilder();
@@ -38,25 +37,26 @@ public class Part1 {
         return sb.toString();
     }
 //[\wa-яА-Я]{4,}
-    public static String sb1(String input) {
-//        String result = input;
-//        Pattern p = Pattern.compile(ONE_WORD);
-//        Matcher m = p.matcher(input);
-//            while (m.find()) {
-//                String word = m.group();
-//                String newWord = word.substring(2);
-//                result = result.replace(m.group(), newWord);
-//            }
+    public static String ChangeInput(String input) {
+        String result = input;
+        Pattern p = Pattern.compile(ONE_WORD, Pattern.MULTILINE);
+        Matcher m = p.matcher(input);
+        StringBuffer sb1 = new StringBuffer();
+            while (m.find()) {
+                String word = m.group();
+                String newWord = word.substring(2);
+                result = result.replace(m.group(), newWord);
+            }
 //-----------------------------------------------------------
-        String REGEX = "[a-zA-Z0-9][\\wа-яёА-ЯЁ]\\b\\w{1,3}\\b";
-        String REPLACE = " ";
-        Pattern pattern = Pattern.compile(REGEX);
-        Matcher matcher = pattern.matcher(input);
-        StringBuffer buffer = new StringBuffer(input);
-        while (matcher.find()) {
-            matcher.appendReplacement(buffer, REPLACE);
-        }
-        matcher.appendTail(buffer);
+//        String REGEX = "[a-zA-Z0-9][\\wа-яёА-ЯЁ]\\b\\w{1,3}\\b";
+//        String REPLACE = " ";
+//        Pattern pattern = Pattern.compile(REGEX);
+//        Matcher matcher = pattern.matcher(input);
+//        StringBuffer buffer = new StringBuffer(input);
+//        while (matcher.find()) {
+//            matcher.appendReplacement(buffer, REPLACE);
+//        }
+//        matcher.appendTail(buffer);
         //-----------------------------------------
 //            Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
 //            Matcher matcher = pattern.matcher(input);
@@ -69,6 +69,6 @@ public class Part1 {
 //                }
 //            }
 //            return sb.toString();
-        return buffer.toString().trim();
+        return result.trim();
     }
 }
